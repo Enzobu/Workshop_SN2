be57ecea1d873e607ae4efb9991bdad47328c9be
Enzobu <enzo.palermo@epsi.fr>
2023-09-20 08:10:16 +0200

    Create link_db.php

---
+++ php/link_db.php
@@ -0,0 +1,36 @@
+<?php
+
+// //constante d'environnement
+// if ($_SERVER['SERVER_NAME'] == 'climat-2030.org') {
+//     define("DBHOST", "bgooodiclimat.mysql.db");
+//     define("DBUSER", "bgooodiclimat");
+//     define("DBPASS", "23marsDonuts");
+//     define("DBNAME", "bgooodiclimat");
+// }
+
+define("DBHOST", "localhost");
+define("DBUSER", "root");
+define("DBPASS", "");
+define("DBNAME", "workshoup_sn2");
+
+//DSN de connection
+$dsn = "mysql:dbname=".DBNAME.";host=".DBHOST;
+
+//On se connecte à la base de données
+try{
+        //On instancie PDO
+    $db = new PDO($dsn, DBUSER, DBPASS);
+
+    //echo "On est connectés";
+
+        //On s'assure d'envoyer les données en utf8
+    $db->exec("SET NAMES utf8");
+
+        //On défini le mode de fetch par default
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
+
+?>
