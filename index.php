1c3e38a95840e3c76898c7101a1bdfa056dbce4d
Enzobu <enzo.palermo@epsi.fr>
2024-02-13 19:12:05 +0100

    updates

--- index.php
+++ index.php
@@ -21,14 +21,14 @@ session_start();
                 <p>Découvrez <span>Learn IT, </span><br>votre pass pour le monde numérique.</p>
             </div>
             <div class="block-1-right">
-                <img src="/images/right-1" alt="" class="r1">
-                <img src="/images/right-2" alt="" class="r2">                
+                <img src="/images/right-1.png" alt="" class="r1">
+                <img src="/images/right-2.png" alt="" class="r2">                
             </div>
         </div>
         <div class="block-2">
             <div class="block-2-left">
-                <img src="/images/left-1" alt="" class="l1">
-                <img src="/images/left-2" alt="" class="l2">
+                <img src="/images/left-1.png" alt="" class="l1">
+                <img src="/images/left-2.svg" alt="" class="l2">
             </div>
             <div class="block-2-right">
                 <p>Profitez de centaines de leçons gratuites et pour tous les niveaux.</p>
@@ -40,7 +40,7 @@ session_start();
                 <h2>Qui sommes nous ?</h2>
             </div>
             <div class="block-3-bas">
-                <img src="/images/bas-1" alt="" class="b1">
+                <img src="/images/bas-1.png" alt="" class="b1">
                     <p>
                         Learn It est bien plus qu'une plateforme de formation en ligne.
                         C'est une initiative passionnée et altruiste qui vise à rendre 
@@ -51,10 +51,10 @@ session_start();
                         créer des opportunités pour un avenir meilleur dans le monde numérique
                     </p>
                 </img>
-                <img src="/images/bas-left" alt="" class="b2">
-                <img src="/images/bas-right" alt="" class="b3">
-                <img src="/images/bas-left" alt="" class="b4">
-                <img src="/images/bas-right" alt="" class="b5">
+                <img src="/images/bas-left.png" alt="" class="b2">
+                <img src="/images/bas-right.png" alt="" class="b3">
+                <img src="/images/bas-left.png" alt="" class="b4">
+                <img src="/images/bas-right.png" alt="" class="b5">
                 
             </div>
         </div>
